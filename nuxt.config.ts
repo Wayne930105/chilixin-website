9f53580ddfd24fced8eabdee9147d53d3e40ee4a
Wayne930105 <168755139+Wayne930105@users.noreply.github.com>
2026-06-03 12:03:15 +0800

    fix: 更新 favicon 和蘋果觸控圖示的路徑及格式

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,7 +38,9 @@ export default defineNuxtConfig({
         { name: 'theme-color', content: '#0D1117' },
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', href: '/images/logo/logo-icon.png' },
+        { rel: 'shortcut icon', type: 'image/png', href: '/images/logo/logo-icon.png' },
+        { rel: 'apple-touch-icon', href: '/images/logo/logo-icon.png' },
         {
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;500;700;900&display=swap',
